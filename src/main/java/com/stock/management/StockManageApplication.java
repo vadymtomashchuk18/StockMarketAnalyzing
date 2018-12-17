31b42d3fe30ac1214d01377ca79d2a2e5b217ba3
vadymtomashchuk18 <vadym.tomashchuk@ukma.edu.ua>
2018-12-17 18:43:34 +0200

    Configured project (Spring Boot, VueJS, Thymeleaf)

---
+++ src/main/java/com/stock/management/StockManageApplication.java
@@ -0,0 +1,14 @@
+package com.stock.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockManageApplication.class, args);
+    }
+
+}
+
