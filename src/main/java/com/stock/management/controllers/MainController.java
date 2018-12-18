5cb7d4e09e523d144f474f2cb767188c59c80a4a
vadymtomashchuk18 <vadym.tomashchuk@ukma.edu.ua>
2018-12-18 17:51:25 +0200

    added some points from api

--- src/main/java/com/stock/management/controllers/MainController.java
+++ src/main/java/com/stock/management/controllers/MainController.java
@@ -4,45 +4,59 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.zankowski.iextrading4j.api.stocks.Quote;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
 import yahoofinance.histquotes2.HistoricalDividend;
 import yahoofinance.histquotes2.HistoricalSplit;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Calendar;
 import java.util.List;
 
 @Controller
-public class MainController {
+public class MainController{
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) throws IOException {
-        // this attribute will be available in the view index.html as a thymeleaf variable
-        model.addAttribute("eventName", "FIFA 2018");
-        // this just means render index.html from static/ area
 
+        model.addAttribute("eventName", "FIFA 2018");
+
+        Calendar from = Calendar.getInstance();
+        Calendar to = Calendar.getInstance();
+        // from 5 years ago
+        from.add(Calendar.YEAR, -1);
         Stock stock = null;
         try {
-            stock = YahooFinance.get("INTC");
+            stock = YahooFinance.get("TSLA", from, to, Interval.MONTHLY);
         } catch (IOException e) {
             e.printStackTrace();
         }
         String namek = stock.getName();
         model.addAttribute("name", namek);
-//        model.addAttribute("name", "Marmeladka");
-//        System.out.println("Man or me " + namek);
         BigDecimal price = stock.getQuote().getPrice();
         model.addAttribute("price", price);
-        BigDecimal change = stock.getQuote().getChangeInPercent();
-        model.addAttribute("change", change);
+        Long shares = stock.getStats().getSharesOutstanding();
+        model.addAttribute("shares", shares);
+        BigDecimal sale = stock.getStats().getPriceSales();
+        model.addAttribute("sale", sale);
+        BigDecimal totalRevenue = stock.getStats().getRevenue();
+        model.addAttribute("totalRevenue", totalRevenue);
         BigDecimal peg = stock.getStats().getPeg();
         model.addAttribute("peg", peg);
         BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
         model.addAttribute("dividend", dividend);
-        List<HistoricalDividend> sstats = stock.getDividendHistory();
+        List<HistoricalQuote> sstats = stock.getHistory(from, to, Interval.MONTHLY);
         model.addAttribute("stats", sstats);
+
+        final IEXTradingClient iexTradingClient = IEXTradingClient.create();
+        final Quote quote = iexTradingClient.executeRequest(new QuoteRequestBuilder()
+                .withSymbol("AAPL")
+                .build());
+        System.out.println(quote);
 
         stock.print();
         return "index";
---
+++ src/main/java/com/stock/management/models/Company.java
@@ -0,0 +1,5 @@
+package com.stock.management.models;
+
+public class Company {
+
+}
